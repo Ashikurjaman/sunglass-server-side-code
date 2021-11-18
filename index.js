f2f2f5ca8933413d588a70182ac03eee7a974893
Ashikurjamane <ashikmpi420@gmail.com>
2021-11-19 04:48:00 +0600

    confirm

--- index.js
+++ index.js
@@ -1,201 +1,172 @@
-const express = require('express')
-const { MongoClient } = require('mongodb');
-const app = express()
-const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 const admin = require("firebase-admin");
-const serviceAccount = require("./lunettes-1e40f-firebase-adminsdk.json");
+
+
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const serviceAccount =JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
-
-require('dotenv').config()
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gshit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function verifyConnection(req, res, next) {
-  if(req.headers?.authrization?.startsWith("Bearer ")){
+  if (req.headers?.authrization?.startsWith("Bearer ")) {
     const token = req.headers.authrization.split(" ")[1];
 
-    try{
+    try {
       const decodedUser = await admin.auth().verifyIdToken(token);
-      req.decodedEmail= decodedUser.email;
-    }
-    catch{
-
-    }
+      req.decodedEmail = decodedUser.email;
+    } catch {}
   }
 
-next()
+  next();
 }
 
-async function run(){
-    try {
-        await client.connect();
-        const database = client.db("sunglass_shop");
-        const userCollection = database.collection("users");
-        const productCollection = database.collection("products")
-        const purchaseCollection = database.collection("purchase")
-        const reviewsCollection = database.collection("addreviews")
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("sunglass_shop");
+    const userCollection = database.collection("users");
+    const productCollection = database.collection("products");
+    const purchaseCollection = database.collection("purchase");
+    const reviewsCollection = database.collection("addreviews");
 
-        app.get("/products", async(req, res) =>{
-          const cursor = productCollection.find({})
-          const products = await cursor.toArray();
-          res.send(products);
-        })
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
 
-        app.post('/products', async (req, res) => {
-          const product = req.body;
-          const result = await productCollection.insertOne(product)
-          res.json(result)
-        });
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.json(result);
+    });
 
-        app.get('/products/:id', async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: ObjectId(id) };
-          const product = await productCollection.findOne(query);
-          res.json(product)
-        })
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.json(product);
+    });
 
-        app.get("/users", async(req, res) =>{
-          const cursor = userCollection.find({})
-          const user = await cursor.toArray();
-          res.send(user);
-        })
-        app.post('/users', async (req, res) => {
-          const user = req.body;
-          const result = await userCollection.insertOne(user)
-          res.json(result)
-          console.log(result);
-        })
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find({});
+      const user = await cursor.toArray();
+      res.send(user);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.json(result);
+      console.log(result);
+    });
 
-        app.get('/addreviews', async (req, res) => {
-          const cursor = reviewsCollection.find({});
-          const review = await cursor.toArray();
-          res.send(review)
-        })
+    app.get("/addreviews", async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const review = await cursor.toArray();
+      res.send(review);
+    });
 
+    app.get("/purchase", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      console.log(query);
+      const cursor = purchaseCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        app.get('/purchase', async (req, res) => {
-          const email = req.query.email;
-          const query = {email: email};
-          console.log(query);
-          const cursor = purchaseCollection.find(query);
-          const result = await cursor.toArray();
-          res.send(result)
-        });
-        
-
-        // reviews post 
-    app.post('/addreviews', async (req, res) => {
+    // reviews post
+    app.post("/addreviews", async (req, res) => {
       const review = req.body;
-      const result = await reviewsCollection.insertOne(review)
-      res.json(result)
+      const result = await reviewsCollection.insertOne(review);
+      res.json(result);
     });
 
-    app.post('/purchase', async (req, res) => {
+    app.post("/purchase", async (req, res) => {
       const purchase = req.body;
-      const result = await purchaseCollection.insertOne(purchase)
-      res.json(result)
+      const result = await purchaseCollection.insertOne(purchase);
+      res.json(result);
     });
-    
 
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
 
-        
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
 
-        app.put('/users', async(req, res) => {
-          const user = req.body;
-          const filter = {email: user.email};
-          const options = {upsert: true};
-          const updateDoc = {$set: user};
-          const result = await userCollection.updateOne(filter, updateDoc, options)
-          res.json(result)
-        })
+    app.delete("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await purchaseCollection.deleteOne(query);
+      res.json(result);
+    });
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.json(result);
+    });
 
-       
-
-        app.get("/users/:email", async (req, res) => {
-          const email = req.params.email;
-          const query = {email: email};
-          const user = await userCollection.findOne(query);
-          let isAdmin = false;
-          if(user?.role === "admin"){
-            isAdmin = true;
-          }
-          res.json({admin: isAdmin});
-        })
-        
-
-
-        app.delete("/purchase/:id", async (req, res) => {
-          const id = req.params.id;
-          const query = {_id:ObjectId(id)};
-          const result = await purchaseCollection.deleteOne(query);
-          res.json(result)
-        })
-
-
-        
-
-
-
-
-         app.put("/users/admin",verifyConnection,  async (req, res) => {
-          // const user = req.body;
-          // const requester = req.decodedEmail;
-          // if(requester) {
-          //   const reqsterAccount = userCollection.findOne({email: requester});
-          //   if(reqsterAccount.role ===admin) {
-          //    const filter = { email:user.email}
-          
-          // const updateDoc = {$set:{role: "admin"}};
-          // const result = await userCollection.updateOne(filter,updateDoc);
-          // res.json(result);
-          //   }
-          // }
-          // else {
-          //   res.status(401)
-          // }
-
-          const user = req.body;
-          const requester = req.decodedEmail;
-          if(requester){
-            const reqsterAccount = await userCollection.findOne({email: requester});
-            if(reqsterAccount.role === "admin"){
-              const filter =  { email: user.email}
-          const updateDoc = {$set:{role: "admin"}};
-          const result = await userCollection.updateOne(filter,updateDoc)
+    app.put("/users/admin", verifyConnection, async (req, res) => {
+      const user = req.body;
+      const requester = req.decodedEmail;
+      if (requester) {
+        const reqsterAccount = await userCollection.findOne({
+          email: requester,
+        });
+        if (reqsterAccount.role === "admin") {
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: "admin" } };
+          const result = await userCollection.updateOne(filter, updateDoc);
           res.json(result);
           console.log(result);
-
-            }
-          }
-          else{
-            res.status(403).json({message:"you do not have permission"})
-          }
-          
-        })
-
-
+        }
+      } else {
+        res.status(403).json({ message: "you do not have permission" });
       }
-    finally {
-        // await client.close();
-    }
+    });
+  } finally {
+    // await client.close();
+  }
 }
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
